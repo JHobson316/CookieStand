18ad067dcc6db2afb12afa8c2ff7cb068257d76a
Jordan Hobson <hobsoncjordan@gmail.com>
2022-06-16 09:16:39 -0500

    pushed for code review

--- sales.js
+++ sales.js
@@ -4,66 +4,80 @@
 // Store the results for each location in a separate array… perhaps as a property of the object representing that location
 // Display the values of each array as unordered lists in the browser
 // Calculating the sum of these hourly totals; your output for each location should look like this:
+let cookieContainer = document.getElementById('sales-table');
+let tableBody = document.createElement('tbody');
+cookieContainer.append(tableBody);
 const hoursOpen = ["7am", "8am", "9am", "10am", "11am", "2am", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"];
-function storeLocation(maxCust, minCust, avrgCookie, cookiesPerHour, customersPerHour, totalDailyCookies){
+function Location(maxCust, minCust, avrgCookie, cookiesPerHour, customersPerHour, totalDailyCookies){
     this.maxCust = maxCust;
     this.minCust = minCust;
     this.cookiesPerHour = cookiesPerHour;
     this.customersPerHour = customersPerHour;
-    this.avrgCookie= avrgCookie;
+    this.avrgCookie = avrgCookie;
     this.totalDailyCookies = totalDailyCookies;
     this.CalCustomersPerHour = function(){
-        for (i = 0; i < hours.length; i++){
+        for (i = 0; i < hoursOpen.length; i++){
             let customersPerHour = this.customersPerHour.push(Math.floor(Math.random(this.minCust - this.minCust + 1)+this.maxCust));
         }
     }
-    this.calCustomersPerHour = function(){
-        for (i=0; i<hours.length; i++){
-            let oneHour = Math.ceil(this.customersPerHour[i] * this.avgeCookie);
+    this.CalCookiesPerHour = function(){
+        for (i=0; i<hoursOpen.length; i++){
+            let oneHour = Math.ceil(this.customersPerHour[i] * this.avrgCookie);
             this.totalDailyCookies += oneHour;
-            this.cookiesPerHour.push(oneHour)
+            this.cookiesPerHour.push(oneHour);
         }
     }
+    this.RenderLoc = function(){
+        let tableRow = document.createElement('tr');
+        let tableData = document.createElement('td');
+        tableData.textContent = `${this.name}`;
+        tableRow.append(tableData);
+    }
+
 }
 let SeattleWashington = new Location(85,15,6.9,[],[]);
+SeattleWashington.CalCustomersPerHour();
+SeattleWashington.CalCookiesPerHour();
+SeattleWashington.RenderLoc();
 let ToledoOhio = new Location(85,15,6.9,[],[]);
 let FlorenceItaly = new Location(85,15,6.9,[],[]);
 let MilanItaly = new Location(85,15,6.9,[],[]);
 let TokyoJapan = new Location(85,15,6.9,[],[]);
-let salesTable = document.getElementById("sales-table")
+let salesTable = document.getElementById("sales-table");
 let tableHeaderEl = document.createElement('th');
 let tableHeaderContainer = document.createElement('thead');
-let tableRow = document.createElement('tr');
-let tablefooter = document.createElement('tfoot')
+
 function renderTableHeader(){
     for (i=0; i<hoursOpen.length;i++){
         let tableHeaderEl = document.createElement('th');
+        let tableRow = document.createElement('tr');
         tableHeaderEl.textContent = `${hoursOpen[i]}`;
         tableRow.append(tableHeaderEl);
     }
     tableHeaderContainer.append(tableRow);
+    console.log(tableRow);
 }
 function renderTableFooter(){
-    for (i=0; i < hours.length + 1; i++ );
+    for (i=0; i < hoursOpen.length + 1; i++ ){
+        let tablefooter = document.createElement('tfoot');
+        tablefooter.textContent = `${this.totalDailyCookies}`;
+        let tableRow = document.createElement('tr');
+        tableRow.append(tablefooter);
+    }
+
 }
 //let ul1 = document.createElement('ul');
 //let ul2 = document.createElement('ul');
 //let ul3 = document.createElement('ul');
 //let ul4 = document.createElement('ul');
 //let ul5 = document.createElement('ul');
-let cookieContainer = document.getElementById('cookies-sold');
-function render(hours){
-    for (i=0; i < hoursOpen.length; i=i+1){
-        let th = document.createElement('th');
-        th.textContent = `${hours[i]}`;
-        cookieContainer.append(th);
-}
-}
-cookieContainer.append();
+
+
 
 //renderSeattle(hoursOpen);
 //renderToledo(hoursOpen);
 //renderFlorence(hoursOpen);
 //renderMilan(hoursOpen);
 //renderTokyo(hoursOpen);
-render();
+console.log(Location(SeattleWashington));
+renderTableHeader();
